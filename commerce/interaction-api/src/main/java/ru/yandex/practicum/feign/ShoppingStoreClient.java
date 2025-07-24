b610fa0a17a42c9fa65862e019f7aaa95848a039
Alexey <e.al.vl@yandex.ru>
2025-07-24 17:40:15 +0300

    Technical task sprint 21 7-spring-cloud-microservices fix ShoppingStoreClient

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/feign/ShoppingStoreClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/feign/ShoppingStoreClient.java
@@ -1,7 +1,8 @@
 package ru.yandex.practicum.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import ru.yandex.practicum.contract.ShoppingStoreOperations;
 
 @FeignClient(name = "shopping-store", path = "/api/v1/shopping-store")
-public interface ShoppingStoreClient {
+public interface ShoppingStoreClient extends ShoppingStoreOperations {
 }
